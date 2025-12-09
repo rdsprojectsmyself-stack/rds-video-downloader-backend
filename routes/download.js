59adcbe1f410a670e78b4aaa3f80fe43e74fef2e
Dhanasekar <rdsprojectsmyself@gmail.com>
2025-12-09 16:31:39 +0530

    Updated backend packages and download route

--- routes/download.js
+++ routes/download.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const downloadController = require('../controllers/downloadController');
 
+// POST /api/download
 router.post('/', downloadController.handleDownload);
 
 module.exports = router;
